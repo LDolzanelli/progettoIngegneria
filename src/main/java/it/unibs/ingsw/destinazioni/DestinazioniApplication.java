7fc1cf70b067f27fcce2f619673ba14440e19966
LDolzanelli <l.dolzanelli@studenti.unibs.it>
2025-05-10 14:30:43 +0200

    Spring time

---
+++ src/main/java/it/unibs/ingsw/destinazioni/DestinazioniApplication.java
@@ -0,0 +1,13 @@
+package it.unibs.ingsw.destinazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinazioniApplication.class, args);
+	}
+
+}
