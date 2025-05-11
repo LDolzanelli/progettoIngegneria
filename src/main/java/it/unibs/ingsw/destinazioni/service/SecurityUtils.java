f7164470701a43106bb66040b6e4ae1034bdd94c
LDolzanelli <l.dolzanelli@studenti.unibs.it>
2025-05-11 19:49:56 +0200

    aggiunto /login screen

---
+++ src/main/java/it/unibs/ingsw/destinazioni/service/SecurityUtils.java
@@ -0,0 +1,22 @@
+package it.unibs.ingsw.destinazioni.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtils {
+    public static String getCurrentUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+
+            if (principal instanceof UserDetails) {
+                return ((UserDetails) principal).getUsername(); // returns "user" in your example
+            } else if (principal instanceof String) {
+                return (String) principal;
+            }
+        }
+        return null;
+    }
+}
--- src/main/java/it/unibs/ingsw/destinazioni/views/HomeView.java
+++ src/main/java/it/unibs/ingsw/destinazioni/views/HomeView.java
@@ -4,15 +4,18 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import it.unibs.ingsw.destinazioni.service.SecurityUtils;
+import jakarta.annotation.security.PermitAll;
 
-@Route("home")
+@Route("")
+@PermitAll
 public class HomeView extends VerticalLayout {
 
     public HomeView() {
 
         add(new H1("Welcome to your new application"));
         add(new Paragraph("This is the home view"));
-
+        add(new Paragraph(SecurityUtils.getCurrentUsername()));
         add(new Paragraph("You can edit this view in src\\main\\java\\it\\unibs\\ingsw\\destinazioni\\views\\HomeView.java"));
 
     }
