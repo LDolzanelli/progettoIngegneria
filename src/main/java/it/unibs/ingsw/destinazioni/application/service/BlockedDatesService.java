5c0b9bb26de6f9f62e4de27d79f0d0521409babc
Lorenzo Dolzanelli <l.dolzanelli@studenti.unibs.it>
2025-07-05 15:34:36 +0200

    aggiunta possibilità di testare con profilo "test" impostando una data qualsiasi (per adesso da hard-coddare su DateTimeConfig.java

--- src/main/java/it/unibs/ingsw/destinazioni/application/service/BlockedDatesService.java
+++ src/main/java/it/unibs/ingsw/destinazioni/application/service/BlockedDatesService.java
@@ -8,6 +8,7 @@ import it.unibs.ingsw.destinazioni.domain.model.BlockedDates;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.Set;
 public class BlockedDatesService implements BlockedDatesUseCase {
 
     private final BlockedDatesRepositoryPort repository;
+    private final Clock clock;
 
     @Override
     public void updateBlockedDates(Set<LocalDate> blockedDates) {
@@ -37,7 +39,7 @@ public class BlockedDatesService implements BlockedDatesUseCase {
 
     @Override
     public Month getMonthToUpdate(){
-        LocalDate today = LocalDate.now();
+        LocalDate today = LocalDate.now(clock);
         int baseMonth;
 
         if (today.getDayOfMonth() < 16) {
