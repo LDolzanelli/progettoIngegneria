76fa3e154186881539dc2e853ab77135387ee33b
LDolzanelli <l.dolzanelli@studenti.unibs.it>
2025-07-12 11:45:19 +0200

    altro fix bug sulle disponibilità volontari

--- src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/DatePickerViewController.java
+++ src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/DatePickerViewController.java
@@ -13,7 +13,7 @@ public class DatePickerViewController {
     @GetMapping("/date-picker")
     public String showDatePickerForm(@RequestParam(defaultValue = "blocked") String mode,
                                      Model model) {
-        model.addAttribute("mode", mode); // "blocked" or "available"
+        model.addAttribute("mode", mode); // "blocked" o "available"
         return "date-picker";
     }
 }
