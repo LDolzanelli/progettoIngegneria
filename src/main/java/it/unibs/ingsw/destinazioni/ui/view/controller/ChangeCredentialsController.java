9f6b159991d25f575902f79dbadb83729cc84617
AbdelazizKhater <abdelazizyoussef2002@gmail.com>
2025-08-16 17:17:19 +0200

    fixato informazioni mancanti nella visualizzazione dei visit type, fix #21

--- src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/ChangeCredentialsController.java
+++ src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/ChangeCredentialsController.java
@@ -1,19 +1,22 @@
 package it.unibs.ingsw.destinazioni.ui.view.controller;
 
-import it.unibs.ingsw.destinazioni.domain.dto.ChangeCredentialsDTO;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
+
+import it.unibs.ingsw.destinazioni.domain.dto.ChangeCredentialsDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
