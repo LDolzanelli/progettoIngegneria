a85bc61f08d405cd63e27e1da9056bce997be654
frnkypp <129514652+frnkypp@users.noreply.github.com>
2025-09-11 15:44:33 +0200

    Corretto view controller della home - Version-2

--- src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/HomeController.java
+++ src/main/java/it/unibs/ingsw/destinazioni/ui/view/controller/HomeController.java
@@ -11,48 +11,47 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class HomeController {
 
-    private final RestTemplate restTemplate = new RestTemplate();
+  private final RestTemplate restTemplate = new RestTemplate();
 
+  @GetMapping({ "/", "/home" })
+  public String home(@AuthenticationPrincipal UserDetails principal, Model model) {
+    String nickname = principal.getUsername();
 
-    @GetMapping("/")
-    public String home(@AuthenticationPrincipal UserDetails principal, Model model) {
-        String nickname = principal.getUsername();
+    String url = "http://localhost:8080/api/";
+    String urlNickname = url + "users/info/" + nickname;
+    String urlId = url + "/users/get-id/" + nickname;
 
-        String url = "http://localhost:8080/api/";
-        String urlNickname = url + "users/info/" + nickname;
-        String urlId = url + "/users/get-id/" + nickname;
+    LoginResponseDTO userInfo;
+    int userId;
 
-        LoginResponseDTO userInfo;
-        int userId;
-
-        try {
-            userInfo = restTemplate.getForObject(urlNickname, LoginResponseDTO.class);
-        } catch (Exception e) {
-            throw new IllegalStateException("Errore nel recupero dell'utente: " + nickname, e);
-        }
-
-        try {
-            userId = restTemplate.getForObject(urlId, Integer.class);
-        } catch (Exception e) {
-            throw new IllegalStateException("Errore nel recupero dell'utente: " + nickname, e);
-        }
-
-        assert userInfo != null;
-        model.addAttribute("username", userInfo.nickname());
-        model.addAttribute("userId", userId);
-        model.addAttribute("role", userInfo.role());
-
-        //Se l'utente è un configuratore, e non esiste ancora un corpo dati, viene reindirizzato ad una pagina
-        //che gli permette di inserire location
-        if (userInfo.role().equalsIgnoreCase("configurator")) {
-            String urlArea = url + "area-of-interest/isEmpty";
-            Boolean exists = restTemplate.getForObject(urlArea, Boolean.class);
-            if(Boolean.TRUE.equals(exists)) {
-                return "redirect:/insert-areas-of-interest";
-            }
-        }
-
-        return "/home";
+    try {
+      userInfo = restTemplate.getForObject(urlNickname, LoginResponseDTO.class);
+    } catch (Exception e) {
+      throw new IllegalStateException("Errore nel recupero dell'utente: " + nickname, e);
     }
-}
 
+    try {
+      userId = restTemplate.getForObject(urlId, Integer.class);
+    } catch (Exception e) {
+      throw new IllegalStateException("Errore nel recupero dell'utente: " + nickname, e);
+    }
+
+    assert userInfo != null;
+    model.addAttribute("username", userInfo.nickname());
+    model.addAttribute("userId", userId);
+    model.addAttribute("role", userInfo.role());
+
+    // Se l'utente è un configuratore, e non esiste ancora un corpo dati, viene
+    // reindirizzato ad una pagina
+    // che gli permette di inserire location
+    if (userInfo.role().equalsIgnoreCase("configurator")) {
+      String urlArea = url + "area-of-interest/isEmpty";
+      Boolean exists = restTemplate.getForObject(urlArea, Boolean.class);
+      if (Boolean.TRUE.equals(exists)) {
+        return "redirect:/insert-areas-of-interest";
+      }
+    }
+
+    return "/home";
+  }
+}
