ff0703049f4f2188d48d7e1094bd7899928b56b6
Lorenzo <lorenzo.dolzanelli@gmail.com>
2025-09-20 10:51:32 +0200

    fix BookingException

--- src/main/java/it/unibs/ingsw/destinazioni/application/exceptions/BookingException.java
+++ src/main/java/it/unibs/ingsw/destinazioni/application/exceptions/BookingException.java
@@ -3,12 +3,16 @@ package it.unibs.ingsw.destinazioni.application.exceptions;
 import it.unibs.ingsw.destinazioni.application.exceptions.codes.BookingErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.StandardException;
 
-@StandardException
 @Getter
 @RequiredArgsConstructor
 public class BookingException extends RuntimeException {
-    BookingErrorCode errorCode;
+    final BookingErrorCode errorCode;
+
+    public BookingException(BookingErrorCode errorCode, String errorMessage)
+    {
+        super(errorMessage);
+        this.errorCode = errorCode;
+    }
 }
 
